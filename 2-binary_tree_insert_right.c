265cc0eb930b91d7e4a656d610fbdfe5af885eee
Delegat-is <kelvinjuma903@gmail.com>
2023-06-01 13:50:05 -0700

    right node

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,37 @@
+#include "binary.h"
+
+/**
+ * binary_tree_t - Entry point
+ *
+ * @parent: pointer to new-node
+ * @value: new_node value
+ * Return: new_node
+*/
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = binary_tree_node(parent, right);
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	if (new_node !=  NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+
+	parent->right = new_node;
+	new_node->right = parent;
+
+	return (new_node);
+}
